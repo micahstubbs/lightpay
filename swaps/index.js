d6d07c21381fd0d0380b2e81eb3bc4d106f0aab0
@micahstubbs <git@micah.fyi>
2026-04-17 09:16:39 -0700

    lightpay-awx.1: Export taproot modules from swaps barrel
    Add taprootClaimTransaction, taprootRefundTransaction, taprootSwapAddress,
    and taprootSwapScript to swaps/index.js so the service layer can import
    them cleanly via destructuring from './../swaps'.

--- swaps/index.js
+++ swaps/index.js
@@ -4,6 +4,10 @@ const swapAddress = require('./swap_address');
 const swapOutput = require('./swap_output');
 const swapScriptDetails = require('./swap_script_details');
 const swapScriptInTransaction = require('./swap_script_in_tx');
+const taprootClaimTransaction = require('./taproot_claim_transaction');
+const taprootRefundTransaction = require('./taproot_refund_transaction');
+const taprootSwapAddress = require('./taproot_swap_address');
+const taprootSwapScript = require('./taproot_swap_script');
 
 module.exports = {
   claimTransaction,
@@ -12,5 +16,8 @@ module.exports = {
   swapOutput,
   swapScriptDetails,
   swapScriptInTransaction,
+  taprootClaimTransaction,
+  taprootRefundTransaction,
+  taprootSwapAddress,
+  taprootSwapScript,
 };
-
